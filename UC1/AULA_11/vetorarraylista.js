4a2cb88add62bf8da4719a4172e72c3ce7cbb3dd
Farius889 <orodrigues.gabrielhotmail@.com>
2026-06-17 10:31:00 -0300

    Aula_12

--- UC1/AULA_11/vetorarraylista.js
+++ UC1/AULA_11/vetorarraylista.js
@@ -8,3 +8,5 @@ let tamanho = nome.length
 console.log(`TAMANHO DA ARRAY: ${tamanho}`)
 console.log(`EXIBINDO DADOS DA ARRAY`)
 console.log(`O mais lindo : ${nome[2]}`)
+
+//Length demonstra a quantidade que há na variável, no caso seria 4
